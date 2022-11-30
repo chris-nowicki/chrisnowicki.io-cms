e4b803343c5021b99daa0cea67fa071e208f9a7b
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-30 16:46:57 -0500

    sanity initial config

---
+++ schemas/tech.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'tech',
+    type: 'document',
+    title: 'Tech',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'link',
+            title: 'Link',
+            type: 'string'
+        },
+        {
+            name: 'show',
+            Title: 'Show',
+            type: 'boolean'
+        }
+    ]
+}
